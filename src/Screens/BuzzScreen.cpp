08d6785fa00ed36f9063d00c3d3786c107d03608
Davide Heller <davide.heller@posteo.net>
2026-09-01 11:54:43 +0200

    change to tripple buzz

--- src/Screens/BuzzScreen.cpp
+++ src/Screens/BuzzScreen.cpp
@@ -12,7 +12,7 @@ void buzz_gpio(){
   pinMode(VIB_MOTOR_PIN, OUTPUT);
   // five cycles of 3 buzzes each
   for (int c = 0; c < 1; c++) {
-    for (int b = 0; b < 1; b++) {
+    for (int b = 0; b < 3; b++) {
       digitalWrite(VIB_MOTOR_PIN, true);
       delay(100);
       digitalWrite(VIB_MOTOR_PIN, false);
